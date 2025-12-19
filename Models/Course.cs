638fb564681d8ea82dea5f76dcf68b92495f3724
sandraudayakumar945 <sandraudayakumar945@gmail.com>
2025-12-19 10:54:16 +0530

    Add files via upload

---
+++ Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentCourseManagement.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseCode { get; set; }
+    }
+}
+
