d55131d86a24b3e3052d0e74445396003ae42313
sandraudayakumar945 <sandraudayakumar945@gmail.com>
2025-12-19 10:44:54 +0530

    Delete MappingModel.cs

--- MappingModel.cs
+++
@@ -1,8 +0,0 @@
-﻿public class StudentCourseMappingModel
-{
-    public int MappingId { get; set; }
-    public string StudentName { get; set; }
-    public string CourseName { get; set; }
-    public string CourseCode { get; set; }
-}
-
