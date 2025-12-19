d8405027aca61a7743c86719452ce4596ba179d9
sandraudayakumar945 <sandraudayakumar945@gmail.com>
2025-12-19 10:38:59 +0530

    Add files via upload

---
+++ MappingModel.cs
@@ -0,0 +1,8 @@
+﻿public class StudentCourseMappingModel
+{
+    public int MappingId { get; set; }
+    public string StudentName { get; set; }
+    public string CourseName { get; set; }
+    public string CourseCode { get; set; }
+}
+
